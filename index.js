7e021b33c7b8b8499defaeddf7fcdda17b3d5196
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-04-21 19:16:23 -0300

    fix: endpoint '/sign-up' concluido

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-import express from "express";
+import express, {json} from "express";
 import cors from "cors";
 import chalk from "chalk";
 
 const serverApp = express();
 serverApp.use(cors());
+serverApp.use(json());
 
-serverApp.get();
+let [ arrayUsuarios, arrayTweets ] = [[ ], [ ]];
 
-serverApp.post();
+/* 
+Users
+{
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}
 
-serverApp.post();
+Tweets
+{
+	username: "bobesponja",
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  tweet: "eu amo o hub",
+}
+*/
+serverApp.post('/sign-up', (req, res)=>{
+    const { username, avatar } = req.body;
+    
+    console.log(chalk.green(username, avatar));
+    console.log("arr", arrayUsuarios);
+    
+    const verificarLogin = arrayUsuarios.find((usuario) => { 
+        return usuario.username === username || usuario.avatar === avatar
+    });
+    
+    console.log("find", verificarLogin);
+    
+    if(verificarLogin !== undefined){
+        res.send('Usuário já existe');
+    }
+    if(verificarLogin === undefined){
+        arrayUsuarios.push({username, avatar});
+        res.send('OK');
+    }
+
+    console.log("arr", arrayUsuarios);
+});
+
+// serverApp.post();
+
+// serverApp.get();
 
 serverApp.listen(5000);
