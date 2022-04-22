23458c1e69dd993c5fd5ca655c5c0767879ab6dd
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-04-22 10:53:43 -0300

    fix: validacao de dados e escopo de rota dinâmica

--- index.js
+++ index.js
@@ -19,11 +19,17 @@ Tweets
 {
 	username: "bobesponja",
 	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-  tweet: "eu amo o hub",
+    tweet: "eu amo o hub",
 }
 */
+
 serverApp.post('/sign-up', (req, res)=>{
     const { username, avatar } = req.body;
+
+    if(username.length === 0 || avatar.length === 0){
+        res.sendStatus(400);
+        return;
+    }
     
     console.log(chalk.red(username, avatar));
     console.log("arr", arrayUsuarios);
@@ -35,11 +41,12 @@ serverApp.post('/sign-up', (req, res)=>{
     console.log("find", verificarLogin);
     
     if(verificarLogin !== undefined){
-        res.send('Usuário já existe');
+        res.sendStatus(422);
     }
     if(verificarLogin === undefined){
         arrayUsuarios.push({username, avatar});
         res.send('OK');
+        res.send(res.statusCode(201));
     }
 
     console.log("arr", arrayUsuarios);
@@ -48,6 +55,11 @@ serverApp.post('/sign-up', (req, res)=>{
 serverApp.post('/tweets', (req, res)=>{
     const { username, tweet } = req.body;
 
+    if(username.length === 0 || tweet.length === 0){
+        res.sendStatus(400);
+        return;
+    }
+
     console.log(chalk.red(username, tweet));
     console.log("arr", arrayTweets);
 
@@ -55,14 +67,21 @@ serverApp.post('/tweets', (req, res)=>{
         return item.username === username && item.tweet === tweet
     });
 
+    const avatarCorrespondente = arrayUsuarios.find((usuario) =>{
+        return usuario.username === username
+    });
+
     console.log("find", verificarTweet);
+    console.log("find user", avatarCorrespondente);
 
     if(verificarTweet !== undefined){
-        res.send('Tweet já existe');
+        res.sendStatus(422);
     }
     if(verificarTweet === undefined){
-        arrayTweets.push({username, tweet});
+        const avatar = avatarCorrespondente.avatar;
+        arrayTweets.push({username, tweet, avatar});
         res.send('OK');
+        res.send(res.statusCode(201));
     }
 
     console.log("arr", arrayTweets);
@@ -76,6 +95,15 @@ serverApp.get('/tweets', (req, res)=>{
         let arrayReduzido = arrayTweets.slice(arrayTweets.length - 10, arrayTweets.length);
         res.send(arrayReduzido);
     }
+});
+
+serverApp.get('/tweets/:idUsuario', (req, res)=>{
+    const { idUsuario } = req.params;
+    const tweetsUsuario = arrayTweets.filter((tweet) =>{
+        return tweet.username === idUsuario
+    });
+
+    res.send(tweetsUsuario);
 });
 
 serverApp.listen(5000);
